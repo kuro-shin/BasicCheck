e5476adcb4d5a5ac30b0ad27abfabbc8b3085812
kuroda_shintaro <sh-kuroda@virtualex.co.jp>
2020-04-23 17:22:44 +0900

    first

---
+++ src/base/Department.java
@@ -0,0 +1,25 @@
+package base;
+
+
+public class Department {
+
+	private String Department_id;
+	private String Department_name;
+
+	public String getDepartment_id() {
+		return Department_id;
+	}
+
+	public void setDepartment_id(String department_id) {
+		Department_id = department_id;
+	}
+
+	public String getDepartment_name() {
+		return Department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		Department_name = department_name;
+	}
+
+}
